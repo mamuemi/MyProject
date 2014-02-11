c239f79a7ae188c0f4a09928cb107104cc222a75
mamuemi <m.muellerminde@gmail.com>
2014-02-10 18:38:42 -0800

    update myProject.py

--- myProject.py
+++ myProject.py
@@ -1,5 +1,5 @@
 # status: 02-10-2014
-# obtain and clean data from web crawl, visualize word count of job profiles as bar plot
+# obtain from web crawl json, clean data (empty, dup entries, stop words, punctuation), do and visualize word count of job profiles as bar plot
 
 import json
 from collections import defaultdict, OrderedDict, Counter
